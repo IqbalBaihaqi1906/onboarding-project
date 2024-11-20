fba55f97b42dbae8ca667d606db1bbedd04ef8d1
Iqbal Baihaqi <iqballbaihaqi1906@gmail.com>
2024-11-20 11:26:26 +0700

    feat: move db cred to env

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Pool } from 'pg';
 
 const dbProvider = {
   provide: 'DB_CONNECTION',
-  useValue: new Pool({
-    user: 'sample_store',
-    host: 'localhost',
-    database: 'sample_store',
-    password: 'sample_store',
-    port: 5434,
-  }),
+  useFactory: () => {
+    const pool = new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: +process.env.DB_PORT,
+    });
+    return pool;
+  },
 };
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes env variables available globally
+    }),
+  ],
   providers: [dbProvider],
   exports: [dbProvider],
 })
